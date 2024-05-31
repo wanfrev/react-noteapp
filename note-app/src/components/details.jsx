5bab63a9d2ef42f001daa6af95163f7057cdedf9
luistargaryen <82510900+luistargaryen@users.noreply.github.com>
2024-05-31 18:59:07 -0400

    Update details.jsx

--- note-app/src/components/details.jsx
+++ note-app/src/components/details.jsx
@@ -1,9 +1,9 @@
 import ".assets/css/details.css";
 
-export const Details = () => {
+export const Details = (setView) => {
     return <div className = "note-details">
         <div className = "details-wrapper">
-            <div className = "details-back-btn">
+            <div className = "details-back-btn" onClick = {() => setView(false)}>
                 <i className = "fa-solid fa-arrow-left"></i>
             </div>
             <h2 className="details-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consequuntur ipsam accusamus, perferendis fuga eum neque similique, quo praesentium debitis animi quam iste deleniti, laboriosam obcaecati porro culpa modi doloremque.</h2>
