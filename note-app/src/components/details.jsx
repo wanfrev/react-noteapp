9f23004ebeddefff788bffa16124897c76215908
luistargaryen <82510900+luistargaryen@users.noreply.github.com>
2024-05-31 18:39:47 -0400

    Add files via upload
    detalles de notas

---
+++ note-app/src/components/details.jsx
@@ -0,0 +1,18 @@
+import ".assets/css/details.css";
+
+export const NoteDetails = () => {
+    return <div className = "note-details">
+        <div className = "details-wrapper">
+            <div className = "details-back-btn">
+                <i className = "fa-solid fa-arrow-left"></i>
+            </div>
+            <h2 className="details-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consequuntur ipsam accusamus, perferendis fuga eum neque similique, quo praesentium debitis animi quam iste deleniti, laboriosam obcaecati porro culpa modi doloremque.</h2>
+            <span className = "details-timeline">{new Date().toDateString()}</span>
+            <div className = "details-body">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta porro, nam consequatur unde sit incidunt exercitationem magni et dolores architecto natus, autem at delectus, optio quo neque dolorum aliquid explicabo.
+                </p>
+            </div>
+        </div>
+    </div>;
+};
