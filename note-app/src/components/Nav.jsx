677ce15261cd0ef72967bbccdccca55bfef2e550
luistargaryen <82510900+luistargaryen@users.noreply.github.com>
2024-05-31 20:39:01 -0400

    Update Nav.jsx

--- note-app/src/components/Nav.jsx
+++ note-app/src/components/Nav.jsx
@@ -1,12 +1,12 @@
 import '../assets/css/nav.css';
 
-export const Nav = () => {
+export const Nav = ({setOpen}) => {
   return (
     <div className='navbar'>
       <div className="nav-wrapper container">
         <span className="logo">Notes</span>
         <div className="nav-options">
-          <div className="nav-icon">
+          <div className="nav-icon" onClick = {() => setOpen(true)}>
           <i className="fa-solid fa-plus"></i>
           </div>
         </div>
