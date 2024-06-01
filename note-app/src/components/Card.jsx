ae2440fecceb326fbb518351408af97a46245d8c
luistargaryen <82510900+luistargaryen@users.noreply.github.com>
2024-05-31 20:13:42 -0400

    Update Card.jsx
    Comentarios dinamicos

--- note-app/src/components/Card.jsx
+++ note-app/src/components/Card.jsx
@@ -1,23 +1,26 @@
 import '../assets/css/card.css'
 
-export const Card = ({setView}) => {
+export const Card = ({setView, note }) => {
   return (
     <div className='note-card'>
       <div className="note-card-wrapper">
         <h2 className="card-title" onClick = {() => setView(true)}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit
+          {note?.title}
         </h2>
         <div className="card-body">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta nisi officiis dolore voluptatum ipsa, optio excepturi iste laborum illum cum aperiam esse, dolor dolorem officia unde? Fuga saepe sed eum?</p>
-        </div>
+          <p>{note?.desc}</p>
         <span className="card-details" onClick = {() => setView(true)}>
           read more
         </span>
         <div className="card-footer">
-          <span className="card-timeline">{new Date().toDateString()}</span>
+          <span className="card-timeline">{note?.createdAt}</span>
           <div className='card-actions'>
-            <div className="action-item"><i className="fa-solid fa-pen-to-square edit"></i></div>
-            <div className="action-item"><i className="fa-solid fa-trash delete"></i></div>
+            <div className="action-item">
+              <i className="fa-solid fa-pen-to-square edit"></i>
+            </div>
+            <div className="action-item">
+              <i className="fa-solid fa-trash delete"></i>
+            </div>
           </div>
         </div>
       </div>
